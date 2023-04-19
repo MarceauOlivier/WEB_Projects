828e2c29e3e07dfda82711b87cf658e8b2d42f24
OlivierMarceau <1868737@cegepmontpetit.ca>
2023-04-19 12:18:06 -0400

    Suite TP3

---
+++ TP2/TP2/Controllers/GestionEnfantController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TP2.Models;
+
+
+namespace TP2.Controllers
+{
+    public class GestionEnfantController : Controller
+    {
+        private DataBase _database { get; set; }
+
+        public GestionEnfantController(DataBase Database)
+        {
+            _database = Database;
+        }
+
+        // GET: GestionEnfantController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GestionEnfantController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
